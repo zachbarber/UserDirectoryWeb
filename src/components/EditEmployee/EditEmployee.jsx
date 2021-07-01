e56ac2ee1b2235d8ec80100b15f5478a8ba1eaf4
Zach Barber <zachbarber321@gmail.com>
2021-07-01 18:00:18 -0400

    sync

--- src/components/EditEmployee/EditEmployee.jsx
+++ src/components/EditEmployee/EditEmployee.jsx
@@ -7,61 +7,80 @@ const EditEmployee = props => {
 
     const history = useHistory();
     const [employeeData, setEmployeeData] = useState([]);
+    const [hasFormErrors, setHasFormErrors] = useState(false);
     const { id, isNew } = useParams();
+    const formErrors = { hasErrors: false };
     const emailRegEx = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
 
     const validateForm = submitEvent => {
-        submitEvent.preventDefault();
-        const errors = { hasErrors: false };
         const formChildren = [...submitEvent.target.children];
+        formErrors.hasErrors = false;
+        for (const error in formErrors) {
+            if (error !== 'hasErrors') {
+                formErrors[error].length = 0;
+            }
+        }
         formChildren.forEach(child => {
-            if (child.id) {
-                errors[`${child.id}`] = {};
+            if (child.id !== 'submitButton') {
                 if ((child.id === 'name' || child.id === 'role') && child.value.length > 50) {
-                    errors[`${child.id}`]['length'] = `${child.id} must be 50 characters or less.`;
+                    if (!formErrors[`${child.id}`]) {
+                        formErrors[`${child.id}`] = [];
+                    }
+                    formErrors[`${child.id}`].push(`<span className='error' id='${child.id}LengthError'>${child.id} must be 50 characters or less.</span>`);
                 }
                 if (child.id === 'name' && !child.value.match(/^[a-zA-Z\s]*$/)) {
-                    errors[`${child.id}`]['format'] = 'name cannot contain numbers.';
+                    if (!formErrors[`${child.id}`]) {
+                        formErrors[`${child.id}`] = [];
+                    }
+                    formErrors[`${child.id}`].push(`<span className='error' id='${child.id}LettersOnlyError'>${child.id} must only contain letters.</span>`);
                 }
                 if (child.id === 'phoneNumber' && (child.value.length !== 10 || !child.value.match(/^[0-9]*$/))) {
-                    errors[`${child.id}`]['format'] = 'must be 10 numbers.';
+                    if (!formErrors[`${child.id}`]) {
+                        formErrors[`${child.id}`] = [];
+                    }
+                    formErrors[`${child.id}`].push(`<span className='error' id='${child.id}NumbersOnlyLengthError'>${child.id} must be 10 numbers.</span>`);
                 }
                 if (child.id === 'emailAddress' && (child.value.length > 50 || !child.value.match(emailRegEx))) {
-                    errors[`${child.id}`]['format'] = 'must be a valid email address of no more than 50 characters.';
+                    if (!formErrors[`${child.id}`]) {
+                        formErrors[`${child.id}`] = [];
+                    }
+                    formErrors[`${child.id}`].push(`<span className='error' id='${child.id}EmailFormatError'>${child.id} must be a valid email address of no more than 50 characters.</span>`);
                 }
-                if (Object.keys(errors[`${child.id}`]).length) {
-                    errors.hasErrors = true;
+                if (formErrors[`${child.id}`]) {
+                    if (formErrors[`${child.id}`].length) {
+                        formErrors.hasErrors = true;
+                    }
                 }
             }
         })
-        if (errors.hasErrors === true) {
-
-            return errors;
+        if(formErrors.hasErrors === true) {
+            setHasFormErrors(true);
         } else {
-
-            return {};
+            setHasFormErrors(false);
         }
+
+        return;
     }
 
     const submitForm = submitEvent => {
         submitEvent.preventDefault();
-        (async () => {
-            const formErrors = validateForm(submitEvent);
-            if (Object.keys(formErrors).length) {
-                console.log(formErrors);
-                console.log(Object.keys(formErrors).length);
-            } else if (!Object.keys(formErrors).length) {
-                console.log('no errors');
-            }
-        })()
-        // (async () => {
-        //     try {
-        //         await axios.post(`/api/employees`, employeeData);
-        //         history.push(`/api/Employee/${employeeData.id}&isNew=false`)
-        //     } catch (err) {
-        //         throw err;
-        //     }
-        // })()
+        validateForm(submitEvent);
+        if (!hasFormErrors) {
+            console.log(submitEvent);
+            console.log('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n')
+            console.log(formErrors);
+            setEmployeeData(employeeData[`${submitEvent.target.id}`] = `${submitEvent.target.value}`)
+            console.log(employeeData);
+            // try {
+            //     await axios.post(`/api/employees`, employeeData);
+            //     history.push(`/api/Employee/${employeeData.id}&isNew=false`)
+            // } catch (err) {
+            //     throw err;
+            // }
+        } else {
+            console.log('formErrors');
+            formErrors.hasErrors = false;
+        };
     }
 
     useEffect(() => {
@@ -72,20 +91,24 @@ const EditEmployee = props => {
             } catch (err) {
                 throw err;
             }
-        })()
+        })();
     }, [id])
 
     return (
         <form id='employeeForm' onSubmit={(e) => submitForm(e)}>
             <label htmlFor='name'>Name</label>
             <input id='name' defaultValue={employeeData?.name} />
+            {hasFormErrors && formErrors['name'] ? () => console.log(formErrors['name']) : null}
             <label htmlFor='role'>role</label>
             <input id='role' defaultValue={employeeData?.role} />
+            {hasFormErrors && formErrors['role'] ? () => console.log(formErrors['role']) : null}
             <label htmlFor='phoneNumber'>phoneNumber</label>
             <input id='phoneNumber' defaultValue={employeeData?.phoneNumber} />
+            {hasFormErrors && formErrors['phoneNumber'] ? () => console.log(formErrors['phoneNumber']) : null}
             <label htmlFor='emailAddress'>emailAddress</label>
             <input id='emailAddress' defaultValue={employeeData?.emailAddress} />
-            <input type='submit' />
+            {hasFormErrors && formErrors['emailAddress'] ? () => console.log(formErrors['emailAddress']) : null}
+            <input id='submitButton' type='submit' />
         </form>
     )
 }
